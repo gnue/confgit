874940dff99207c4b53b95304a946c4f617ee127
gnue <gnue@so-kukan.com>
2013-02-09 13:23:25 +0900

    confgit_spec.rb に repo サブコマンドのテストを実装

--- spec/confgit_spec.rb
+++ spec/confgit_spec.rb
@@ -1,6 +1,7 @@
 require File.expand_path('../spec_helper', __FILE__)
 require 'confgit'
 
+
 describe Confgit do
 	def confgit(*argv)
 		begin
@@ -19,10 +20,65 @@ describe Confgit do
 	end
 
 	describe "repo" do
-		it "repo"
-		it "repo REPO"
-		it "repo -d REPO"
-		it "repo -D REPO"
+		it "repo" do
+			out, err = capture_io { confgit 'repo' }
+			out.must_equal "* #{@hostname}\n"
+		end
+
+		it "repo REPO" do
+			name = '_foo_'
+			out, err = capture_io {
+				confgit 'repo', name
+				confgit 'repo'
+			}
+
+			out.must_equal <<-EOD.gsub(/^\t+/,'')
+				* #{name}
+				  #{@hostname}
+			EOD
+		end
+
+		it "repo -d REPO (not current)" do
+			name1 = '_foo_'
+			name2 = '_bar_'
+			out, err = capture_io {
+				confgit 'repo', name1
+				confgit 'repo', name2
+				confgit 'repo', '-d', name1
+				confgit 'repo'
+			}
+
+			out.must_equal <<-EOD.gsub(/^\t+/,'')
+				* #{name2}
+				  #{@hostname}
+			EOD
+		end
+
+		it "repo -d REPO (current)" do
+			name = '_foo_'
+			out, err = capture_io {
+				confgit 'repo', name
+				confgit 'repo', '-d', name
+				@abort == "'#{name}' is current repository!\n"
+				confgit 'repo'
+			}
+
+			out.must_equal <<-EOD.gsub(/^\t+/,'')
+				* #{name}
+				  #{@hostname}
+			EOD
+		end
+
+		it "repo -D REPO" do
+			name = '_foo_'
+			out, err = capture_io {
+				confgit 'repo', name
+				confgit 'repo', '-D', name
+				confgit 'repo'
+			}
+
+			out.must_equal "* #{@hostname}\n"
+		end
 	end
 
 	describe "add" do
