91cccb338f4080e6a59700b48ca8a84ef2901c09
gnue <gnue@so-kukan.com>
2013-02-12 22:22:28 +0900

    VERSION の生成方法を変更

--- lib/confgit/version.rb
+++ lib/confgit/version.rb
@@ -1,4 +1,4 @@
 module Confgit
 	LONG_VERSION = File.read(File.expand_path('../../../VERSION', __FILE__)).chomp
-	LONG_VERSION.scan(/^([0-9.]+)/) { |match| VERSION = match[0] }
+	VERSION = LONG_VERSION.gsub(/-.*$/, '')
 end
