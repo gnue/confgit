0f33873492c69df9cf04e08be653c7ad43dcacfd
gnue <gnue@so-kukan.com>
2013-02-14 18:15:54 +0900

    git のタグでバージョン番号の前に v がつく場合でも正しくバージョン番号を取出せるように修正

--- lib/confgit/version.rb
+++ lib/confgit/version.rb
@@ -1,4 +1,4 @@
 module Confgit
 	LONG_VERSION = File.read(File.expand_path('../../../VERSION', __FILE__)).chomp
-	VERSION = LONG_VERSION.gsub(/-.*$/, '')
+	VERSION = LONG_VERSION.gsub(/(-.*|\(.*)$/, '')
 end
